739b31feec9756cefbde1721da24eb01cce8cb71
khadkabobby <108467070+khadkabobby@users.noreply.github.com>
2022-10-29 09:18:18 +0545

    Add existing project files

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -9,7 +9,6 @@ import Linkedin from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
 import thumbup from "../../img/thumbup.png";
 import Crown from "../../img/crown.png";
 import glassesimoji from "../../img/glassesimoji.png";
